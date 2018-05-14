db45cac038be8dae58a0d95723dc8fa1a7354339
mymac <@mail>
2018-05-14 21:38:47 +0800

    check result

--- index.js
+++ index.js
@@ -14,19 +14,38 @@ function convert(message, from, to) {
   
 assert(program.args.length > 0, "you must input a seed or some mnemonic")
 
+function addr2mn(addr, isASCII) {
+  let hexAddr = isASCII
+    ? convert(addr, 'ascii', 'hex')
+    : addr
+  let mn = mnemonic.encode(hexAddr)
+  return mn
+}
+
+function mn2addr(mn, isASCII) {
+  let hexAddr = mnemonic.decode(mn)
+  let addr = isASCII
+    ? convert(hexAddr, 'hex', 'ascii')
+    : hexAddr
+  return addr
+}
+
 if (program.args.length === 1) {
   // encode address to mnemonic
-  let addr = program.ascii
-    ? convert(program.args[0], 'ascii', 'hex')
-    : program.args[0]
-  let mn = mnemonic.encode(addr)
+  let mn = addr2mn(program.args[0], program.ascii)
+  let reAddr = mn2addr(mn.slice(0), program.ascii)
+  if (program.ascii) {
+    assert.strictEqual(reAddr, program.args[0])
+  }
+  else { // hex number
+    assert.strictEqual(parseInt(reAddr, 16), parseInt(program.args[0], 16))
+  }
   console.log(mn.join(' '))
 }
 else {
   // decode mnemonic to address
-  let hexAddr = mnemonic.decode(program.args)
-  let addr = program.ascii
-    ? convert(hexAddr, 'hex', 'ascii')
-    : hexAddr
+  let addr = mn2addr(program.args.slice(0), program.ascii)
+  let reMN = addr2mn(addr, program.ascii)
+  assert.deepEqual(reMN, program.args)
   console.log(addr)
 }
