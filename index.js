f2e9725443aad6cf1497e929af532227584afdbd
mymac <@mail>
2018-05-19 15:19:37 +0800

    add cli

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const program = require('commander')
 const assert = require('assert')
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .option('--ascii', 'input/output is ASCII')
   .parse(process.argv);
 
